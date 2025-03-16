f646a7d192857455eccd93e52fb7571fbc8cc257
mrDani <darnnielnology@gmail.com>
2025-03-15 21:51:20 -0500

    fixed bugs

--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import styled from 'styled-components';
-import { HeroImageProps } from './HeroImage.types';
+import { HeroImageProps } from './HeroImage.types'; 
 
-const HeroContainer = styled.div<{ imageSrc: string }>`
-  background-image: url(${({ imageSrc }) => imageSrc});
+const HeroContainer = styled.div<{ $imageSrc: string }>`
+  background-image: url(${({ $imageSrc }) => $imageSrc});
   background-size: cover;
   background-position: center;
   padding: 50px;
@@ -14,9 +15,9 @@ const HeroImage: React.FC<HeroImageProps> = ({
   imageSrc,
   title,
   description,
-  disabled,
+  disabled = false,
 }) => (
-  <HeroContainer imageSrc={imageSrc} style={{ opacity: disabled ? 0.5 : 1 }}>
+  <HeroContainer $imageSrc={imageSrc} style={{ opacity: disabled ? 0.5 : 1 }}>
     <h1>{title}</h1>
     <p>{description}</p>
   </HeroContainer>
